dc0052ca5ec82e94a5a96894f7b000b0b798cdc2
George Ridgway <georger361@gmail.com>
2018-01-30 14:18:21 +0000

    added skeleton project, should be able to be able to import this into eclipse

---
+++ cluedo_game/src/cluedo_game/test.java
@@ -0,0 +1,10 @@
+package cluedo_game;
+
+public class test {
+	public static void main (String[] args) {
+		System.out.println("ghjghjghj");
+		System.out.println("Kelsey Check");
+		System.out.println("George Checdadsask");
+		System.out.println("Kelsey Branch Check");
+	}
+}
