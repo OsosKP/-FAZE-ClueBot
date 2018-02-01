2d087c6d6d255b632dff069f41b7caf77186fa0f
George Ridgway <georger361@gmail.com>
2018-02-01 14:58:39 +0000

    Merge branch 'kelsey' of https://github.com/UCD-COMP20050/-FAZE-ClueBot into kelsey

--- cluedo_game/src/cluedo_game/FloorSquare.java
+++ cluedo_game/src/cluedo_game/FloorSquare.java
@@ -1,45 +1,39 @@
 package cluedo_game;
 
 public class FloorSquare implements BoardSquare {
-    /*
-        Instance Variables
-     */
+    //Instance Variables
     private Token playerOn;         // The player token currently on the square
     private final Token spawnPoint; // The player who spawns on this square
     private int[] location;         // Coordinates of this square on the board
 
-    
+    //
+    //Constructor
+    //
     public FloorSquare(int x, int y, Token spawnPoint) {
-		this.location[0] = x;
-		this.location[1] = y;
-		this.spawnPoint = spawnPoint;
-	}
-    
-	public FloorSquare(int x, int y) {
-		this.location[0] = x;
-		this.location[1] = y;
-		this.spawnPoint = null;
-		this.playerOn = null;
-	}
+	    this.location[0] = x;
+	    this.location[1] = y;
+  		this.spawnPoint = spawnPoint;
+  	}
 
-	public void setPlayerOn(Token playerOn) {
-        this.playerOn = playerOn;
-    }
+  	public FloorSquare(int x, int y) {
+  		this.location[0] = x;
+  		this.location[1] = y;
+  		this.spawnPoint = null;
+  		this.playerOn = null;
+  	}
 
-    public void removePlayerOn() {
-        this.playerOn = null;
-    }
-
-    public Token getPlayerOn() {
-        return playerOn;
-    }
-    
-    public Token getSpawnPoint() {
-        return spawnPoint;
-    }
-
+    //
+    //Accessors
+    //
+    public Token getPlayerOn() {return playerOn;}
+    public Token getSpawnPoint() {return spawnPoint;}
     @Override
-    public int[] getLocation() {
-        return location;
-    }
+    public int[] getLocation() {return location;}
+
+    //
+    //Mutators
+    //
+  	public void setPlayerOn(Token playerOn) {this.playerOn = playerOn;}
+    public void removePlayerOn() {this.playerOn = null;}
+
 }
