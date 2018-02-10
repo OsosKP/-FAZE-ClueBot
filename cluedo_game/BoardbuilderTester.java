0ffd5d2da4dadbbb9966af7d7e12b140350a062e
George Ridgway <georger361@gmail.com>
2018-02-10 20:28:49 +0000

    BoardBuilder is not initializing properly || Finished the BoardBuilder tester class

--- cluedo_game/BoardbuilderTester.java
+++ cluedo_game/BoardbuilderTester.java
@@ -1,16 +1,13 @@
 package cluedo_game;
 
 public class BoardbuilderTester {
-   
-    public static void main (String[] agrs) {
-    	
-    	/* Does the board create correctly */
-    	BoardBuilder builderTest = new BoardBuilder();
-    	
-    	/* Does the board re-create properly */
-    	builderTest.RecreateBoard();
 
-    	/* Can we get all the rooms without breaking everything */
+    public BoardBuilder buildTest() {
+    	BoardBuilder builderTest = new BoardBuilder();
+    	return builderTest;
+    }
+    
+    public void testRooms(BoardBuilder builderTest) {
     	Room Ballroom = builderTest.getBallroom();
         Room Conservatory = builderTest.getConservatory();
         Room DiningRoom = builderTest.getDiningRoom();
@@ -21,17 +18,43 @@ public class BoardbuilderTester {
         Room Kitchen = builderTest.getKitchen();
         Room Study = builderTest.getStudy();
         Room Cellar = builderTest.getCellar();
+    }
+    
+    public void printMap(BoardBuilder builderTest) {
         
         BoardSquare[][] BoardSquareTest = builderTest.returnBoard();
         
         /* Does the board correctly print? */
         for (int rows = 0; rows < 24; rows++) {
         	for (int cols = 0; cols < 25; cols++) {
-        		if (BoardSquareTest[rows][cols].getSquareType().equals("Room")) {
-        			System.out.println("We goot someathing");
+        		if (BoardSquareTest[rows][cols] instanceof FloorSquare) {
+        			System.out.print(" | Floor | ");
+        		}
+        		else if (BoardSquareTest[rows][cols] instanceof WallSquare) {
+        			System.out.print(" | Wall  | ");
+        		}
+        		else if (BoardSquareTest[rows][cols] instanceof EntrySquare) {
+        			System.out.print(" | Entry | ");
+        		}
+        		else if (BoardSquareTest[rows][cols] instanceof Room) {
+        			System.out.print(" | Room  | ");
+        		}
+        		else {
+        			System.out.print(" | ??    |");
         		}
         	}
+        	System.out.print("\n");
         }
+    }
+    
+    
+    
+    public static void main (String[] agrs) {
+    	BoardbuilderTester testMe = new BoardbuilderTester();
+    	BoardBuilder testing = testMe.buildTest();
+    	
+    	testMe.testRooms(testing);
+    	testMe.printMap(testing);
     }
 
 }
