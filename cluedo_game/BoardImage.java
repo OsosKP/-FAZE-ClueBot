f450d721de34067aa34b8de5a0bfed579e8b8bf5
George Ridgway <georger361@gmail.com>
2018-02-11 14:22:50 +0000

    deleted test file

--- cluedo_game/BoardImage.java
+++ cluedo_game/BoardImage.java
@@ -1,109 +1,81 @@
 package cluedo_game;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
+import javax.imageio.ImageIO;
 
 public class BoardImage {
-	ImagePanel test = new ImagePanel();
-	GridPanel test1 = new GridPanel();
 
-	/**
-	 * Creates and returns a JPanel based on the board image
-	 * @return
-	 */
-	public void returnBoardPanel() {
-		/* Creating the JFrame, setting default operations */
-		JFrame frame = new JFrame("JPanel Example");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public BoardImage(BufferedImage bi) {
 
-		JLabel testing = new JLabel();
-		testing = test.createImage();
+        JFrame frame = new JFrame("Test Bufferedimage");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int w = bi.getWidth();
+        int h = bi.getHeight();
+        frame.setSize(w,h);
+        frame.setVisible(true);
+        int step = h/23;
+        JPanel p = new JPanel(new GridLayout(23,23));
+        p.setOpaque(false);
 
-		testing.setLayout(new BorderLayout());
-		frame.setContentPane(testing);
+        int count = 0;
+        for (int ii=0; ii<w; ii+=step) {
+            for (int jj=0; jj<h; jj+=step) {
+                // This is it - GET THE SUB IMAGE
+                System.out.println("Getting subimage coords: " + jj + ", " + ", " + ii + ", " +step + ", "+ step);
+                Image icon = bi.getSubimage(jj, ii, 23, 23);
+                if (1==1) {
+                    JButton button = new JButton(new ImageIcon(icon));
 
-		frame.add(test1.paintMe());
+                    // remove the border - indicate action using a different icon
+                    button.setBorder(null);//THIS IS IMPORTANT BECAUSE IT COMBINES THE SUB
 
+                    // make a 'pressed' icon..
+                    BufferedImage iconPressed = new BufferedImage(step,step,BufferedImage.TYPE_INT_ARGB);
+                    Graphics g = iconPressed.getGraphics();
+                    g.drawImage(icon, 0, 0, p);
+                    g.setColor(Color.GREEN);
+                    g.drawRoundRect(
+                            0, 0,
+                            iconPressed.getWidth(p)-1, iconPressed.getHeight(p)-1,
+                            12, 12);
+                    g.dispose();
+                    button.setPressedIcon(new ImageIcon(iconPressed));
 
-		frame.pack();
-		frame.setResizable(false);
-		frame.setVisible(true);
+                    // make it transparent
+                    //button.setContentAreaFilled(false);
 
-	}
+                    button.setActionCommand(""+count);
+                    button.addActionListener(new ActionListener(){
+                        @Override
+                        public void actionPerformed(ActionEvent ae) {
+                            System.out.println(ae.getActionCommand());
+                        }
+                    });
 
-	public static void main (String[] agrs) {
-		BoardImage test = new BoardImage();
-		test.returnBoardPanel();
-	}
-}
+                    p.add(button);
+                }
+                count++;
+            }
+        }
+        JPanel center = new JPanel(new GridBagLayout());
+        center.setBackground(Color.BLACK);
+        center.add(p);
+        frame.add(center);//FOR SOME REASON IT WORKS IN A JOPTIONPANE BUT NOT IN A JFRAME
+    }
 
-class ImagePanel extends JPanel{
-
-	public JLabel createImage() {
-		JLabel temp = new JLabel();
-		ImageIcon icon = new ImageIcon(this.getImage("/boardEdit.jpeg"));
-
-		temp.setIcon(icon);
-		temp.setBounds(0, 0, 552, 575); //needed in order for 24x25 to work
-
-		return temp;
-	}
-
-	public Image getImage(String filePath) {
-		int width, height;
-		Image tempImage = null;
-		/* Now we need to try to load the image into the file system */
-		try {
-			/* Loads the image, and assigns it to the tempImage var */
-			URL imagePath = BoardImage.class.getResource(filePath);
-			tempImage = Toolkit.getDefaultToolkit().getImage(imagePath);
-
-
-		}
-		catch(Exception e){ //if the filePath does not exist, or something else messed up
-			System.err.println("We were not able to load the requested image form the given filePath: " + "\n" + filePath);
-		}
-		return tempImage;
-	}
-
-}
-
-class GridPanel extends JPanel{
-	private JLabel[][] grid;
-
-	public GridPanel paintMe(){
-		this.setBounds(0, 0, 552, 575); //needed in order for 24x25 to work
-		this.fillGrid();
-		this.setOpaque(false);
-		return this;
-	}
-
-	public void fillGrid() {
-		this.setLayout(new GridLayout(24, 25));
-
-		grid = new JLabel[24][25];
-
-		for (int i = 0; i < 24; i++) {
-			for (int j = 0; j < 25; j++) {
-				grid[i][j] = new JLabel();
-				grid[i][j].setBorder(new LineBorder(Color.BLACK));
-				grid[i][j].setOpaque(false);
-				this.add(grid[i][j]);
-			}
-		}
-
-	}
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://i.imgur.com/A01Gmjw.jpg");//http://i.stack.imgur.com/SNN04.png
+        final BufferedImage bi = ImageIO.read(url);
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                new BoardImage (bi);
+            }
+        });
+    }
 }
