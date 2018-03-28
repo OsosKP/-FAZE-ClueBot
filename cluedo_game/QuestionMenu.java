358a842b74574e6cd4c4fd52cd9281e54efa4d07
George Ridgway <georger361@gmail.com>
2018-03-28 16:36:37 +0100

    trying to learn gridBagLayout

--- cluedo_game/QuestionMenu.java
+++ cluedo_game/QuestionMenu.java
@@ -4,7 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,13 +34,45 @@ public class QuestionMenu {
 		currentContainer = new JPanel();
 		layout = new GridBagLayout();
 		gbc = new GridBagConstraints();
+		currentContainer.setLayout(layout);
 		
 		JLabel characterLabel = new JLabel("Select a Character");
-		this.addItems(characterLabel, 0, 2, 1, 1);
 		
+		
+		
+		JLabel greenPic = new JLabel("hello");
+
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		
+		convertImg("green");
+		greenPic.setText("what is going wrong here?");
+			
+		gbc.weightx = 0.5;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		layout.setConstraints(characterLabel, gbc);
+		currentContainer.add(characterLabel);
+		
+		//this.addItems(characterLabel, 0, 0, 1, 1);
+		//this.addItems(greenPic, 1, 3, 1, 1);
+
 		this.currentDisplay.add(currentContainer);
 		this.currentDisplay.setVisible(true);
 	}
+	private void convertImg(String name) {
+		BufferedImage readImage = null;
+		try {
+			if (name.equals("green")) {
+				readImage = ImageIO.read(new File("characterCards/Green.png"));
+				System.out.println("I am actually printing stuff rn!");
+			}		
+		} catch (IOException e) {
+			System.err.println("Bad stuff just happened");
+		}
+		
+	}
+	
 	
 	/* Method that adds a component to the container jPanel, based on given params */
 	public void addItems(Component addMe, int gridx, int gridy, int gridwidth, int gridhight) {
@@ -51,6 +89,7 @@ public class QuestionMenu {
 		layout.setConstraints(addMe, gbc);
 		currentContainer.add(addMe);
 	}
+	
 }
 
 
