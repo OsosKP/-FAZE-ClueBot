f0cbc184e618d018a1d56dac8ece35128b5fe1ca
George Ridgway <georger361@gmail.com>
2018-03-28 15:20:31 +0100

    figured out how to get gridbagLayout to work -- TODO: get the menu actually working

--- cluedo_game/QuestionMenu.java
+++ cluedo_game/QuestionMenu.java
@@ -5,7 +5,9 @@ import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class QuestionMenu {
@@ -25,13 +27,29 @@ public class QuestionMenu {
 	
 		currentContainer = new JPanel();
 		layout = new GridBagLayout();
+		gbc = new GridBagConstraints();
 		
+		JLabel characterLabel = new JLabel("Select a Character");
+		this.addItems(characterLabel, 0, 2, 1, 1);
+		
+		this.currentDisplay.add(currentContainer);
 		this.currentDisplay.setVisible(true);
 	}
 	
 	/* Method that adds a component to the container jPanel, based on given params */
-	public void addItems(Component addMe, int gtidx, int gridy, int gridwidth, int gridhight) {
+	public void addItems(Component addMe, int gridx, int gridy, int gridwidth, int gridhight) {
+		/*	gridx = basically the row variable in our layout
+		 *  gridy = works as the column variable
+		 * 
+		 */
+		this.gbc.gridx = gridx;
+		this.gbc.gridy = gridy;
 		
+		this.gbc.gridwidth = gridwidth;
+		this.gbc.gridheight = gridhight;
+		
+		layout.setConstraints(addMe, gbc);
+		currentContainer.add(addMe);
 	}
 }
 
