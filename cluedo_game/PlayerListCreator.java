3a0229d34a7740fb576d35896708089e9869efca
Sciencetod0 <joshuaiking56@gmail.com>
2018-04-08 17:01:04 +0100

    BoardImage.removePlayer for strings and Tokens

--- cluedo_game/PlayerListCreator.java
+++ cluedo_game/PlayerListCreator.java
@@ -479,6 +479,7 @@ public class PlayerListCreator {
             }
             else {
             	playerList.printList();
+            	UserInterface.myImg.initPlayers();
                 GameLogic.createGame();
                 GameLogic.getUi().pressStartGameButton();
             }
